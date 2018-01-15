9e7cbe30c2eb921cf309350839df1f0a2a97ff72
Tomas Molina <jtml.mass@gmail.com>
2018-01-14 22:55:21 -0400

    Added click changing picture

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
-
 function multiply(num1,num2){
     var result = num1 * num2
     return result;
@@ -12,4 +9,14 @@ multiply(0.5,3);
 
 document.querySelector('h1').onclick = function() {
     alert('Ouch! Stop poking me!');
+}
+
+var myImage = document.querySelector('img');
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png'){
+        myImage.setAttribute('src','images/safari.png');
+    }else{
+        myImage.setAttribute('src','images/firefox-icon.png');
+    }
 }
